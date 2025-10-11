0a8d3cb9fb35797ff744f161c0e19169654206e2
Apoorv-Yadav-ux <apoorvyadav2022@vitbhopal.ac.in>
2025-10-11 17:50:34 +0530

    Controllers part-II

--- src/routes/user.routes.js
+++ src/routes/user.routes.js
@@ -1,5 +1,5 @@
 import { Router } from "express";
-import { registerUser } from "../controllers/user.controller.js";   // ✅ add this
+import { registerUser } from "../controllers/user.controller.js";   
 
 const router = Router();
 
